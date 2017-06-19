aa9d6116057a3f1e30d95eaaba9662294a2144fe
HTyan <yxanuzhe12006@126.com>
2017-06-19 20:44:01 +0800

    1

--- src/com/etc/fms/company/dao/CompanyMapper.java
+++ src/com/etc/fms/company/dao/CompanyMapper.java
@@ -1,17 +1,21 @@
 package com.etc.fms.company.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.etc.fms.company.entity.Company;
 
+@Repository(value = "companydao")
 public interface CompanyMapper {
-    int deleteByPrimaryKey(Integer cid);
 
-    int insert(Company record);
+	int deleteByPrimaryKey(Integer cid);
 
-    int insertSelective(Company record);
+	int insert(Company record);
 
-    Company selectByPrimaryKey(Integer cid);
+	int insertSelective(Company record);
 
-    int updateByPrimaryKeySelective(Company record);
+	Company selectByPrimaryKey(Integer cid);
 
-    int updateByPrimaryKey(Company record);
+	int updateByPrimaryKeySelective(Company record);
+
+	int updateByPrimaryKey(Company record);
 }
